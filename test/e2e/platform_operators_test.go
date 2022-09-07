ac3c894134a242b2ffa9bbbb287ba7c8fbf86463
timflannagan <timflannagan@gmail.com>
2022-09-07 16:07:57 -0400

    test/e2e: Remove the magiccatalog library
    Signed-off-by: timflannagan <timflannagan@gmail.com>

--- test/e2e/platform_operators_test.go
+++ test/e2e/platform_operators_test.go
@@ -3,12 +3,10 @@ package e2e
 import (
 	"context"
 	"fmt"
-	"path/filepath"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	rukpakv1alpha1 "github.com/operator-framework/rukpak/api/v1alpha1"
-	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
@@ -18,88 +16,14 @@ import (
 	platformtypes "github.com/openshift/platform-operators/api/v1alpha1"
 )
 
-const (
-	dataBaseDir = "testdata"
-)
-
 var _ = Describe("platform operators controller", func() {
 	var (
-		ns  *corev1.Namespace
 		ctx context.Context
 	)
 	BeforeEach(func() {
 		ctx = context.Background()
-		ns = SetupTestNamespace(c, genName("e2e-"))
 	})
-	AfterEach(func() {
-		Expect(c.Delete(ctx, ns)).To(BeNil())
-	})
-	// TODO(tflannag): We _really_ need to fix OLM's registry connection logic as waiting on
-	// the CatalogSource resource to report a "READY" connection state accounts for the majority
-	// of the overall e2e suite time, despite the underlying registry Pod serving the connection
-	// already for 20/30s sometimes.
-	When("sourcing content from a single catalogsource", func() {
-		var (
-			catalog *MagicCatalog
-		)
-		BeforeEach(func() {
-			provider, err := NewFileBasedFiledBasedCatalogProvider(filepath.Join(dataBaseDir, "prometheus.v0.1.0.yaml"))
-			Expect(err).To(BeNil())
-
-			catalog = NewMagicCatalog(c, ns.GetName(), "prometheus", provider)
-			Expect(catalog.DeployCatalog(ctx)).To(BeNil())
-		})
-		AfterEach(func() {
-			Expect(catalog.UndeployCatalog(ctx)).To(BeNil())
-		})
-		When("a platformoperator has an update available", func() {
-			var (
-				po *platformv1alpha1.PlatformOperator
-			)
-			BeforeEach(func() {
-				po = &platformv1alpha1.PlatformOperator{
-					ObjectMeta: metav1.ObjectMeta{
-						GenerateName: "prometheus-operator",
-					},
-					Spec: platformv1alpha1.PlatformOperatorSpec{
-						Package: platformv1alpha1.Package{
-							Name: "prometheus-operator",
-						},
-					},
-				}
-				Expect(c.Create(ctx, po)).To(BeNil())
-			})
-			AfterEach(func() {
-				Expect(c.Delete(ctx, po)).To(BeNil())
-			})
-			It("should initially select the v0.1.0 package", func() {
-				Eventually(func() bool {
-					bi := &rukpakv1alpha1.BundleDeployment{}
-					if err := c.Get(ctx, types.NamespacedName{Name: po.GetName()}, bi); err != nil {
-						return false
-					}
-					return bi.Spec.Template.Spec.Source.Image.Ref == "quay.io/operatorhubio/prometheus:v0.47.0"
-				}).Should(BeTrue())
-			})
-			When("the catalog has been updated and a new olm.bundle version is available", func() {
-				BeforeEach(func() {
-					updatedProvider, err := NewFileBasedFiledBasedCatalogProvider(filepath.Join(dataBaseDir, "prometheus.v0.2.0.yaml"))
-					Expect(err).To(BeNil())
-
-					Expect(catalog.UpdateCatalog(ctx, updatedProvider)).To(BeNil())
-				})
-				It("should result in the v0.2.0 package being installed", func() {
-					Eventually(func() bool {
-						bi := &rukpakv1alpha1.BundleDeployment{}
-						if err := c.Get(ctx, types.NamespacedName{Name: po.GetName()}, bi); err != nil {
-							return false
-						}
-						return bi.Spec.Template.Spec.Source.Image.Ref == "quay.io/operatorhubio/prometheus:v0.47.0--20220413T184225"
-					}).Should(BeTrue())
-				})
-			})
-		})
-
+	When("sourcing content from the redhat-operators catalog source", func() {
 		When("a platformoperator has been created", func() {
 			var (
 				po *platformv1alpha1.PlatformOperator
