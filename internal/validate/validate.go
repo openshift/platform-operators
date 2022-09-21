c27e6cc5fb7e71734497578e0929d68ad682f47b
Tyler Slaton <tyslaton@redhat.com>
2022-09-21 16:36:38 -0400

    fix: update validate logic to not check self
    Prior to this commit there was an issue where the UniquePackage funtion
    was checking if PlatformOperators were unique, including itself. As a
    result it would fail after every invocation.

--- internal/validate/validate.go
+++ internal/validate/validate.go
@@ -9,18 +9,25 @@ import (
 	platformv1alpha1 "github.com/openshift/api/platform/v1alpha1"
 )
 
+// UniquePackage checks that the provided PlatformOperator contains a unique spec.package.name
+// when compared to the other PlatformOperators existing on cluster.
 func UniquePackage(ctx context.Context, cli client.Client, po *platformv1alpha1.PlatformOperator) error {
 	existingPlatformOperators := &platformv1alpha1.PlatformOperatorList{}
 	if err := cli.List(ctx, existingPlatformOperators); err != nil {
 		return err
 	}
 
-	for _, otherPO := range existingPlatformOperators.Items {
-		if otherPO.Spec.Package.Name == po.Spec.Package.Name {
+	for _, existingPO := range existingPlatformOperators.Items {
+		// check whether we're processing the same PlatformOperator as the parameter
+		if existingPO.GetName() == po.GetName() {
+			continue
+		}
+
+		if existingPO.Spec.Package.Name == po.Spec.Package.Name {
 			return fmt.Errorf(
 				"%v spec.package.name is not unique, conflicts with PlatformOperator %v",
 				po.Name,
-				otherPO.Name,
+				existingPO.Name,
 			)
 		}
 	}
