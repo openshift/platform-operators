d90f4f7b3746ad019ecc834b25f8f9933ef5b0ef
timflannagan <timflannagan@gmail.com>
2022-09-27 16:27:10 -0400

    test/e2e: Introduce a bash script that gathers testing artifacts
    Signed-off-by: timflannagan <timflannagan@gmail.com>

---
+++ test/e2e/collect-ci-artifacts.sh
@@ -0,0 +1,43 @@
+#! /bin/bash
+
+set -o pipefail
+set -o nounset
+set -o errexit
+
+: "${KUBECONFIG:?}"
+: "${ARTIFACT_DIR:?}"
+: "${KUBECTL:=kubectl}"
+
+function ensure_kubectl() {
+    if ! which ${KUBECTL} &> /dev/null; then
+        echo "cannot find kubectl binary in \$PATH"
+        exit 1
+    fi
+}
+
+function collect_artifacts() {
+    commands=()
+    commands+=("get co platform-operators-aggregated -o yaml")
+    commands+=("get platformoperators -o yaml")
+    commands+=("get bundledeployments -o yaml")
+    commands+=("get bundles -o yaml")
+
+    echo "Storing the test artifact output in the ${ARTIFACT_DIR} directory"
+    for command in "${commands[@]}"; do
+        echo "Collecting ${command} output..."
+        COMMAND_OUTPUT_FILE=${ARTIFACT_DIR}/${command// /_}
+
+        ${KUBECTL} ${command} >> "${COMMAND_OUTPUT_FILE}"
+    done
+}
+
+function main() {
+    echo "Using the ${KUBECTL} kubectl binary"
+    echo "Using the ${ARTIFACT_DIR} output directory"
+    mkdir -p "${ARTIFACT_DIR}"
+
+    ensure_kubectl
+    collect_artifacts
+}
+
+main
