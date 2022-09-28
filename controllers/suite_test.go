cc4f38013001c823c284411d428acee830051fd6
timflannagan <timflannagan@gmail.com>
2022-09-28 18:05:49 -0400

    controllers: Refactor the envtest suite initialization
    Signed-off-by: timflannagan <timflannagan@gmail.com>

--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -17,12 +17,19 @@ limitations under the License.
 package controllers
 
 import (
-	"path/filepath"
+	"context"
 	"testing"
+	"time"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	"k8s.io/client-go/kubernetes/scheme"
+	configv1 "github.com/openshift/api/config/v1"
+	operatorsv1alpha1 "github.com/operator-framework/api/pkg/operators/v1alpha1"
+	rukpakv1alpha1 "github.com/operator-framework/rukpak/api/v1alpha1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/rest"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
@@ -32,43 +39,68 @@ import (
 	//+kubebuilder:scaffold:imports
 )
 
-// These tests use Ginkgo (BDD-style Go testing framework). Refer to
-// http://onsi.github.io/ginkgo/ to learn more about Ginkgo.
-
-var k8sClient client.Client
-var testEnv *envtest.Environment
+var (
+	cfg     *rest.Config
+	testEnv *envtest.Environment
+	ctx     context.Context
+	cancel  context.CancelFunc
+	scheme  *runtime.Scheme
+	c       client.Client
+)
 
 func TestAPIs(t *testing.T) {
 	RegisterFailHandler(Fail)
+	SetDefaultEventuallyTimeout(30 * time.Second)
+	SetDefaultEventuallyPollingInterval(1 * time.Second)
 
 	RunSpecs(t, "Controller suite")
 }
 
 var _ = BeforeSuite(func() {
 	logf.SetLogger(zap.New(zap.WriteTo(GinkgoWriter), zap.UseDevMode(true)))
+	ctx, cancel = context.WithCancel(context.Background())
 
 	By("bootstrapping test environment")
 	testEnv = &envtest.Environment{
-		CRDDirectoryPaths:     []string{filepath.Join("..", "config", "crd", "bases")},
+		CRDDirectoryPaths:     []string{"../manifests", "../vendor/github.com/openshift/api/config/v1/"},
 		ErrorIfCRDPathMissing: true,
 	}
 
-	cfg, err := testEnv.Start()
+	var err error
+	cfg, err = testEnv.Start()
 	Expect(err).NotTo(HaveOccurred())
 	Expect(cfg).NotTo(BeNil())
 
-	err = platformv1alpha1.Install(scheme.Scheme)
+	scheme = runtime.NewScheme()
+
+	err = platformv1alpha1.Install(scheme)
 	Expect(err).NotTo(HaveOccurred())
+
+	err = configv1.Install(scheme)
+	Expect(err).NotTo(HaveOccurred())
+
+	err = rukpakv1alpha1.AddToScheme(scheme)
+	Expect(err).To(BeNil())
+
+	err = operatorsv1alpha1.AddToScheme(scheme)
+	Expect(err).To(BeNil())
+
+	err = clientgoscheme.AddToScheme(scheme)
+	Expect(err).To(BeNil())
+
+	err = corev1.AddToScheme(scheme)
+	Expect(err).To(BeNil())
+
+	c, err = client.New(cfg, client.Options{
+		Scheme: scheme,
+	})
+	Expect(err).To(BeNil())
 
 	//+kubebuilder:scaffold:scheme
-
-	k8sClient, err = client.New(cfg, client.Options{Scheme: scheme.Scheme})
-	Expect(err).NotTo(HaveOccurred())
-	Expect(k8sClient).NotTo(BeNil())
-
 })
 
 var _ = AfterSuite(func() {
+	cancel()
 	By("tearing down the test environment")
 	err := testEnv.Stop()
 	Expect(err).NotTo(HaveOccurred())
